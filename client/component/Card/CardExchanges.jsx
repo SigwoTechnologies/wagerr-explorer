a7a3366a7aaf145301cb7882d6319c16a7f7096e
sigwo <steven@sigwo.com>
2019-08-14 21:16:39 -0500

    Added CoinSuper and TurtleNetwork per David

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -6,10 +6,10 @@ import Card from './Card';
 const CardExchanges = () => (
   <Card title="Exchanges">
     <a href="https://crex24.com/exchange/WGR-BTC" target="_blank">Crex24</a><br />
-    <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
+    <a href="https://coinsuper.com/" target="_blank">CoinSuper</a><br />
     <a href="https://chainex.io/" target="_blank">ChainEx</a><br />
     <a href="https://folex.io/" target="_blank">Folex</a><br />
-    <a href="https://www.ionomy.com/" target="_blank>Ionomy</a><br />
+    <a href="https://www.ionomy.com/" target="_blank">Ionomy</a><br />
     <a href="https://livecoin.net/" target="_blank">LiveCoin</a><br />
   </Card>
 );
